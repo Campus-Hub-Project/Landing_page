ac64368127b4d4b9e1e00c6812afa63c76c8312d
GabrielVDantas <gvd2808@gmail.com>
2024-11-06 15:51:59 -0300

    teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import { Button } from '@/src/components/ui/button'
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/src/components/ui/card'
+import { Button } from '../components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../components/ui/card'
+
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
